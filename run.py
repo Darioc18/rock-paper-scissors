326ecd748eb20f4520e288ca663e0848cbb6cf49
Dario <dariocovella@gmail.com>
2023-08-27 21:22:06 +0100

    Add initial input and request name

--- run.py
+++ run.py
@@ -2,6 +2,11 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+print("Welcome to this Rock, Paper, Scissors Game")
+player_name = input("Insert your name: ")
+# games_number = input("How many games? ")
+
+
 def get_player_choice():
 
 def get_computer_choice():
