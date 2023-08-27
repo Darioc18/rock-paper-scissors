147a579f2bee1ec466771ddb267966b10523de9f
Dario <dariocovella@gmail.com>
2023-08-27 21:18:48 +0100

    Add core structure for the game

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def get_player_choice():
+
+def get_computer_choice():
+
+def determine_winner():
+
+def main():
