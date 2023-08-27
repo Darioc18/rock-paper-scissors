e46c75be8b802e0ad5c6f1f52a5e809a1077ab36
Dario <dariocovella@gmail.com>
2023-08-27 21:32:36 +0100

    Add function to get player input and validate the input is 'r','p', or 's'

--- run.py
+++ run.py
@@ -8,9 +8,23 @@ player_name = input("Insert your name: ")
 
 
 def get_player_choice():
+    while True:
+        try:
+            player_choice = input("Enter your choice ('r' for rock, 'p' for paper, 's' for scissors): ").strip().lower()
+            if player_choice not in ['r', 'p', 's']:
+                raise ValueError(f"You entered '{player_choice}'. Choose 'r' for rock, 'p' for paper, 's' for scissors.")
+            print(player_choice)
+            break
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.\n")
+                
 
-def get_computer_choice():
 
-def determine_winner():
+# def get_computer_choice():
+
+# def determine_winner():
 
 def main():
+    get_player_choice()
+
+main()
