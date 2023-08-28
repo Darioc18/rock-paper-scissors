c764f1552a054fcad3ebec93e8efb54e36dba66d
Dario <dariocovella@gmail.com>
2023-08-28 01:37:55 +0100

    Create the play_again function to allow restarting or ending the game upon its completion.

--- run.py
+++ run.py
@@ -4,21 +4,22 @@
 
 import random
 
-print("Welcome to this Rock, Paper, Scissors Game")
+print("Welcome to Rock, Paper, Scissors Game")
 player_name = input("Insert your name: ")
 # games_number = input("How many games? ")
 
 
 def get_player_choice():
-    try:
-        player_choice = input("Enter your choice ('r' for rock, 'p' for paper, 's' for scissors): ").lower()
-        if player_choice not in ['r', 'p', 's']:
-            raise ValueError(f"You entered '{player_choice}'. Choose 'r' for rock, 'p' for paper, 's' for scissors")
-        choice_mapping = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors'}
-        print(f"Your choice: {choice_mapping[player_choice]}")
-        return player_choice
-    except ValueError as e:
-        print(f"Invalid data: {e}.\n Please try again.\n")
+    while True:
+        try:
+            player_choice = input(f"Hey {player_name}! Enter your choice ('r' for rock, 'p' for paper, 's' for scissors): ").lower()
+            if player_choice not in ['r', 'p', 's']:
+                raise ValueError(f"You entered '{player_choice}'. Choose 'r' for rock, 'p' for paper, 's' for scissors")
+            choice_mapping = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors'}
+            print(f"Your choice: {choice_mapping[player_choice]}")
+            return player_choice
+        except ValueError as e:
+            print(f"Invalid data: {e}.\n Please try again.\n")
                 
 
 
@@ -43,13 +44,32 @@ def determine_winner(player_choice, computer_choice):
         print("Computer wins!")
         return "Computer wins!"
 
-
+def play_again():
+    while True:
+        play_again = input(f"Hey {player_name}! Do you want to play again? (y/n): ").lower()                  
+        if play_again == "y":
+                return True
+        elif play_again == "n":            
+            print("Thank you for playing!")
+            return False
+        
+        # try:
+        #     if play_again != "y" and "n":
+        #         raise ValueError(f"you entered '{play_again}'. Choose 'y' for yes or 'n' for no")
+        #     return play_again
+        # except ValueError as e:
+        #     print(f"Invalid data: {e}.\n")
 
 
 def main():
-    player_choice = get_player_choice()
-    computer_choice = get_computer_choice()
-    determine_winner(player_choice, computer_choice)
+    while True: 
+        player_choice = get_player_choice()
+        computer_choice = get_computer_choice()
+
+        determine_winner(player_choice, computer_choice)
+
+        if not play_again():
+            break         
     
 
 main()
